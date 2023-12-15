e473144d2d27c90190165f06fa5e82b2d08428b1
Aji Fatur <ajifatur2@gmail.com>
2023-12-15 14:14:44 +0700

    Force https, fix bugs

--- resources/views/admin/pegawai/inactive.blade.php
+++ resources/views/admin/pegawai/inactive.blade.php
@@ -30,7 +30,7 @@
                         <tbody>
                             @foreach($pegawai as $p)
                             <tr>
-                                <td><a class="{{ $p->status_kerja->status == 0 ? 'text-danger' : '' }}" href="{{ route('admin.pegawai.detail', ['id' => $p->id]) }}">{{ $p->nip }}</a></td>
+                                <td><a class="{{ $p->status_kerja->status == 0 ? 'text-danger' : '' }}" href="{{ route('admin.pegawai.detail', ['id' => $p->id]) }}">'{{ $p->nip }}</a></td>
                                 <td>{{ $p->nama }}</td>
                                 <td>{{ $p->gelar_depan }}</td>
                                 <td>{{ $p->gelar_belakang }}</td>
